51a275a9013d71cb5694542578626f297ec241a8
Gvbmastrr <gnangndong@gaming.tech>
2022-11-07 18:44:26 +0100

    Exercices d'entrainements

---
+++ ex.py
@@ -0,0 +1,27 @@
+#DEBUT
+#ex 1
+assertionFinal = (((365 * 3) / (24 - (16 - 8))) * (230) > 12000) == (10 * 1250 < 12000)
+pta= (((365 * 3) / (24 - (16 - 8))) * (230) > 12000) 
+#15740.625 > 12000  
+#true
+ptb= (10 * 1250 < 12000) 
+#12500 < 12000
+#false
+ptfinal = pta == ptb #false
+#ex2
+assertionFinalDeux = (((365 * 3) / (4 - (12 - 8))) * (230) > 12000) == (10 * 1250 < 12000)
+pta = (((365 * 3) / (4 - (12 - 8))) * (230) > 12000) 
+#false
+ptb = (10 * 1250 < 12000)
+#false
+ptfinaldeux = false == false #true
+def retournerSixPlusTrois ():
+    return 6 + 3
+def retournerYPlusX (x):
+    return 6 + x
+ECRIRE "BONJOUR MONDE"
+PRINT " HELLO WORLD"
+retournerSixPlusTrois()
+retournerSixPlusX(9)
+print("Hello World !")
+#FIN
