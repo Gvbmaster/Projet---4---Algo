3e35c3358d9438a2585bc9cda0593a9df8050813
Gvbmastrr <gnandndong@gaming.tech>
2022-11-08 19:29:48 +0100

    Compteur + Recursive

--- ex-2.py
+++ ex-2.py
@@ -4,13 +4,33 @@
 #DEBUT
 #Définir un la fonction par un argument
 def miniGame(letterResult):
-    #Attribuer une valeur d'input à n
+    #on met un compteur
+    compteur = 1
     #Tant que la lettre H n'est pas renvoyée
     while input() != letterResult:
+        compteur = compteur+1
         print("Mauvaise lettre")
         #On donne à nouveau une valeur d'input à n
     #Retourne le resultat disant que n est égale à l'argument.
-    return "Vous avez Gagnez"
+    return "Vous avez Gagnez, après " +str(compteur)+" essaie(s)"
 print (miniGame("h"))
 
+#FIN
+
+#DEBUT
+#Définir un la fonction par un argument
+def miniGame(letterResult):
+    #Vérifier si la lettre est la bonne
+    #Si le joueur à perduS
+    if letterResult != input("Entrer une lettre"):
+        #Afficher si le joueur à perdu
+        print("Vous avez perdu")
+        #Ajouter une valeur au compteur
+        return miniGame(letterResult) + 1
+    #Sinon le joueur  gagné
+    else :
+        print ("Vous avez gagnez")
+        return 1 
+
+print(miniGame("h"))
 #FIN
