f7c71e8d8e2aee741ae606ed436fe79524727509
Gvbmastrr <gnangndong@gaming.tech>
2022-11-09 09:35:28 +0100

    exercice 2 corrigé

--- ex-2.py
+++ ex-2.py
@@ -52,23 +52,39 @@ len(tableau) #Renvoie la longueur de tableau renvoir 5
 
 #Exercie 1 
 #Faire une fonction qui concatene 2 chaines de caractere, les séparants par une virgule
-def virgule(x,y):
-    motPlusVirgule= x + ',' + '' + y
-    print(motPlusVirgule)
-    return
-print("Gabriel", "Nang Ndong")
-
+def concatWithComma(strA,strB):
+    #Je m'assure que strA soit bien de type str
+    stringifiedA= str(strA)
+    #Je m'assure que strB soit bien de type str
+    stringifiedB= str(strB)
+    #Retourne strA +',' + strB
+    return strA +','+ strB
+concatWithComma(17, "épée")
 #Exercice 2
 #Faire une fonction qui itère sur tous les index d'un tableau renvoyant une chaine de caractere
 #Avec l'ensemblres des occurences d'un chiffre e.g.:
 #Pour tableau = [0,1,1,1,0,1,1,0,1]
 #la fonction(tableau,0) doit renvoyer "0,4,7" n'hesitez pas à implementer la premiere fonction ,)
 tableau=[0,1,1,1,0,1,1,0,1]
-
-def indexTableau(x):
-    print(tableau(x))
-
-indexTableau(1)
+#Définir la fonction findIndex qui itère sur tableau, cherchant l'index des différentes occurences de x
+def findIndex(tableau,x):
+    #Définir un index de départ
+    i = 0
+    #Définir chaineRetour telle qu'une chaine de caractère vide
+    chaineRetour= ""
+    #Tant que i est différent du nombre d'elt dans le tableau 
+    while (i != len(tableau)):
+        #Alors j'attribue a une variable la valeur de tableau à l'index i
+        selected = tableau[i]
+        #Si selected est égale à x
+        if selected == x :
+            #Alors j'assigne le retour de  concatWithComma tel que: concatWithComma (chainerRetour, i) à chaineRetour
+            chaineRetour = concatWithComma(chaineRetour, i)
+        #J'incrémente i de 1
+        i= i + 1
+    #Retourne la chaineRetour
+    return chaineRetour
+findIndex(tableau, 1)
 #Exercice 3
 #Renvoyer / Afficher un message
 
